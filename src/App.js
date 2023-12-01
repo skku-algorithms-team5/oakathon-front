d507c3ea000c2fb864413b963b29373b7fbb900a
Gyunseo Lee <rbstj0311@naver.com>
2023-12-01 19:57:55 +0900

    chore(fe): App.js에 entry point 주석으로 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
+import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">{/* 여기에 코드 작성 */}</div>;
 }
 
 export default App;
